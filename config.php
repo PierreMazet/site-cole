b1a09370b102a121681be44a64602a4dbc40bfad
PierreMazet <p.mazet16@hotmail.fr>
2015-04-26 18:04:19 +0200

    création des pages

---
+++ config.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include("head.php"); ?>
+
+
+
+<body>
+
+<?php include("nav.php"); ?>
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Page Heading/Breadcrumbs -->
+        <div class="row">
+           <iframe src="http://192.168.1.27/upload.htm"width="1000" height="1000"></iframe>
+        </div>
+        <!-- /.row -->
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
---
+++ fluxvideo.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include("head.php"); ?>
+
+
+
+<body>
+
+<?php include("nav.php"); ?>
+
+
+    <!-- Page Content -->
+    <div class="container">
+
+        <!-- Intro Content -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h2 class="page-header">Our Team</h2>
+            </div>
+            <iframe src="http://192.168.1.27/mjpeg.cgi" width="48%" height="450"></iframe>
+            <iframe src="http://192.168.1.27/image/jpeg.cgi" width="48%" height="450"></iframe>
+        </div>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
